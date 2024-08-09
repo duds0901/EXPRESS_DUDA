5b5a0d5dde6a05f33b1ce44e14ded0f015eab0be
Duda <137117671+duds0901@users.noreply.github.com>
2024-08-09 12:37:35 -0300

    Add files via upload

--- server6.js
+++ server6.js
@@ -1,36 +1,57 @@
-//incorporar  respostas a partir 
 
-const express = require("express");
-const app = express();
+//incorporar respostas a partir de outros arquivos 
+const express = require('express');
+const app = express()
 
 app.listen(3000,()=>{
-    console.log("Servidor no ar mano!!!");
-
+    console.log("Servidor no ar mano!!!")
 })
 
-//criar arquivo functions
+//criar arquivo functions 
 
-
-//importar as funções
+//importar as funçoes
 const funcoes = require('./functions')
 
-app.get('/', (req,res)=>{
-    res.send("teste com arquivos externos");
+app.get('/',(req,res)=>{
+    res.send("Teste com arquivos externos");
 })
 
-
+//rota para a funçao add 
 app.get('/add/:a/:b',(req,res)=>{
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
-    const resultado = funcoes.add(a,b);
-    res.send(`A soma de ${a} com ${b} é ${resultado}`);
-})
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)  
 
-//criar rota com hífen
+    const resultado = funcoes.add(a,b); 
 
-app.get('/add/:a-:b',(req,res)=>{
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
-    const resultado = funcoes.add(a,b);
-    res.send(`A soma de ${a} com ${b} é ${resultado}`);
-})
+    res.send(`A soma de ${a} com ${b} é ${resultado}`) 
+}) 
+
+//criar a rota para sub (com hífen)
+app.get('/sub/:a/:b',(req,res)=>{
+    const b = parseInt(req.params.b)
+    const a = parseInt(req.params.a)  
+
+    const resultado = funcoes.sub(b,a); 
+
+    res.send(`A subtração de ${b} com ${a} é ${resultado}`) 
+}) 
+
+//criar rota com divisao 
+app.get('/div/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)  
+
+    const resultado = funcoes.div(a,b); 
+
+    res.send(`A divisão de ${a} com ${b} é ${resultado}`) 
+}) 
+
+//criar rota com multiplicaçao 
+app.get('/mul/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)  
+
+    const resultado = funcoes.mul(a,b); 
+
+    res.send(`A multiplicação de ${a} com ${b} é ${resultado}`) 
+}) 
